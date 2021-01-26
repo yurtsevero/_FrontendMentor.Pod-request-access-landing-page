b2d331a38661271737e21866fb94a27a18668d3e
o.yrtsvr@gmail.com <o.yrtsvr@gmail.com>
2021-01-27 00:07:25 +0100

    last design changes

--- app.js
+++ app.js
@@ -6,14 +6,19 @@ let emailValidation = /[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/;
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let email = document.getElementById("email").value;
-  if (email == "") {
+  if (!email == "") {
+    if (emailValidation.test(email)) {
+      error.innerHTML = "Your request was submitted successfully.";
+      error.classList.add("show");
+      error.style.color = "Green";
+      setTimeout(hideSuccessfullMsganClearField, 5000);
+    } else {
+      error.innerHTML = "Email not valid!!!";
+      error.classList.add("show");
+      setTimeout(hideErrorMsg, 5000);
+    }
+  } else {
     error.innerHTML = "Email can not be empty!!!";
-    error.classList.add("show");
-    setTimeout(hideErrorMsg, 5000);
-  }
-
-  if (!emailValidation.test(email)) {
-    error.innerHTML = "Email not valid!!!";
     error.classList.add("show");
     setTimeout(hideErrorMsg, 5000);
   }
@@ -21,4 +26,9 @@ form.addEventListener("submit", (e) => {
 
 function hideErrorMsg() {
   error.innerHTML = "";
+}
+function hideSuccessfullMsganClearField() {
+  error.innerHTML = "";
+  document.getElementById("email").value = "";
+  error.style.color = "Red";
 }
